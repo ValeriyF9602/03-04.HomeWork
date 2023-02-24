ee4f2b14f703ae258a8c74d5b01bfdbe3b399616
Valeriy Fateev <Valerka9602@mail.ru>
2023-02-24 15:28:55 +0800

    Решение задачи 25

--- ex25/Program.cs
+++ ex25/Program.cs
@@ -7,4 +7,41 @@
 
 **************************************************************************/
 
+int ReadInt(string text, int defoult)
+{
+    Console.Write(text + $" (по умолчанию {defoult}): ");
+    string? result = Console.ReadLine();
+    if (result != "")
+    {
+        return Convert.ToInt32(result);
+    }
+    return defoult;
+}
 
+void PowerNum(int number, int power)
+{
+    int result = number;
+
+    if (power > 0)
+    {
+        for (int i = 1; i < power; i++)
+        {
+            result *= number;
+        }
+
+        Console.WriteLine($"Число {number} в степени {power}: {result}");
+    }
+    else
+    {
+        Console.WriteLine("Степень должна быть натуральной"); // "... и возводит число A в НАТУРАЛЬНУЮ степень B."
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Возведение числа A в натуральную степень B");
+
+int num = ReadInt("Введите число A", 0);
+int pow = ReadInt("Введите натуральную степень B", 1); // "... и возводит число A в НАТУРАЛЬНУЮ степень B."
+PowerNum(num, pow);
+
+Console.WriteLine();
